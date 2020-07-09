a354ad409bb927db050a7e9ae7744b3cbf40ca9e
YennNgoc <ngocvy.nts@gmail.com>
2020-07-09 11:58:42 +0700

    scrolled logging

--- interface.py
+++ interface.py
@@ -1,80 +1,111 @@
-from tkinter import Tk, Frame, BOTH, X, Y, NONE, LEFT, Label, Entry, Text, W
+from tkinter import Tk, Frame, BOTH, X, Y, NONE, LEFT, Label, Entry, Text, W, scrolledtext
 import tkinter as tk
 
-#LOAD DATA FUNCTION
 
-#DATA UPDATE FUNCTION
-ip_addr = "127.0.0.1" 
+
+
+'''ip_addr = "127.0.0.1" 
 sv_port = "80" 
 cl_port = "52352" 
 stt = "Connected" 
 log = "ABCDEF"
+'''
 
 class Inerface(Frame):
     def __init__(self, parent):
         #khung giao dien chinh
         Frame.__init__(self, parent, background="#FFFFFF")
         self.parent = parent
-        self.initUI(ip_addr, sv_port, cl_port, stt, log)
+        self.initUI()
   
-    def initUI(self, ip_addr, sv_port, cl_port, stt, log):
+    def initUI(self):
         #cai dat thong tin: ten, khoi tao de ve len khung giao dien
         self.parent.title("[MMT_18_5]209_214_215")
         self.pack(fill=BOTH, expand=1)
         #frame IP Address - Port Server
-        frame1 = Frame(self)
-        frame1.pack(fill= X)
+        self.frame1 = Frame(self)
+        self.frame1.pack(fill= X)
  
-        addr_lb = Label(frame1, text="IP Address:", width=10, anchor=W)
-        addr_lb.pack(side=LEFT, padx=5, pady=5)           
+        self.addr_lb = Label(self.frame1, text="IP Address:", width=10, anchor=W)
+        self.addr_lb.pack(side=LEFT, padx=5, pady=5)           
  
-        addr_entry = Text(frame1, width=12, height=1)
-        addr_entry.pack(side=LEFT,fill=NONE, padx=5, pady=5, expand=0)
-        addr_entry.insert(tk.END, ip_addr)
+        self.addr_entry = Text(self.frame1, width=12, height=1)
+        self.addr_entry.pack(side=LEFT,fill=NONE, padx=5, pady=5, expand=0)
+        #self.addr_entry.insert(tk.END, ip_addr)
 
         #Server Port
-        port_sv_lb = Label(frame1, text="Server Port:")
-        port_sv_lb.pack(side=LEFT, padx=30, pady=5)           
+        self.port_sv_lb = Label(self.frame1, text="Server Port:")
+        self.port_sv_lb.pack(side=LEFT, padx=30, pady=5)           
  
-        port_sv_entry = Text(frame1, width=7, height=1)
-        port_sv_entry.pack(side=LEFT,fill=NONE, padx=5, pady=5, expand=0)
-        port_sv_entry.insert(tk.END, sv_port)
+        self.port_sv_entry = Text(self.frame1, width=7, height=1)
+        self.port_sv_entry.pack(side=LEFT,fill=NONE, padx=5, pady=5, expand=0)
+        #self.port_sv_entry.insert(tk.END, sv_port)
 
         #frame Status
-        frame2 = Frame(self)
-        frame2.pack(fill= X)
+        self.frame2 = Frame(self)
+        self.frame2.pack(fill= X)
  
-        stt_lb = Label(frame2, text="Status:", width=10, anchor=W)
-        stt_lb.pack(side=LEFT, padx=5, pady=5)           
+        self.stt_lb = Label(self.frame2, text="Status:", width=10, anchor=W)
+        self.stt_lb.pack(side=LEFT, padx=5, pady=5)           
  
-        stt_entry = Text(frame2, width=12, height=1)
-        stt_entry.pack(side=LEFT,fill=NONE, padx=5, pady=5, expand=0)
-        stt_entry.insert(tk.END, stt)
+        self.stt_entry = Text(self.frame2, width=12, height=1)
+        self.stt_entry.pack(side=LEFT,fill=NONE, padx=5, pady=5, expand=0)
+        #self.stt_entry.insert(tk.END, stt)
 
         #Client Port
-        port_cl_lb = Label(frame2, text="Client Port:")
-        port_cl_lb.pack(side=LEFT, padx=30, pady=5)           
+        self.port_cl_lb = Label(self.frame2, text="Client Port:")
+        self.port_cl_lb.pack(side=LEFT, padx=30, pady=5)           
  
-        port_cl_entry = Text(frame2, width=7, height=1)
-        port_cl_entry.pack(side=LEFT,fill=NONE, padx=5, pady=5, expand=0)
-        port_cl_entry.insert(tk.END, cl_port)
+        self.port_cl_entry =  Text(self.frame2, width=7, height=1)
+        self.port_cl_entry.pack(side=LEFT,fill=NONE, padx=5, pady=5, expand=0)
+        #self.port_cl_entry.insert(tk.END, cl_port)
 
         #frame Log
-        frame3 = Frame(self)
-        frame3.pack(fill= BOTH, expand=True)
+        self.frame3 = Frame(self)
+        self.frame3.pack(fill= BOTH, expand=True)
  
-        log_lb = Label(frame3, text="Logging:", width=10, anchor=W)
-        log_lb.pack(side=LEFT, padx=5, pady=5)           
+        self.log_lb = Label(self.frame3, text="Logging:", width=10, anchor=W)
+        self.log_lb.pack(side=LEFT, padx=5, pady=5)           
  
-        log_entry = Text(frame3, width=47, height=12)
-        log_entry.pack(side=LEFT, fill=NONE, padx=5, pady=5, expand=0)
-        log_entry.insert(tk.END, log)
-       
-def UI_server(ip_addr, sv_port, cl_port, stt, log):        
+        self.log_entry = scrolledtext.ScrolledText(self.frame3, width=47, height=12)
+        self.log_entry.pack(side=LEFT, fill=NONE, padx=5, pady=5, expand=0)
+        #self.log_entry.insert(tk.END, log+'\n')
+    
+    #DATA UPDATE FUNCTION 
+    def updateUI(self, ip_addr, sv_port, cl_port, stt, log):      
+        #frame IP Address - Port Server
+        self.addr_entry.delete(1.0,tk.END)
+        self.addr_entry.insert(tk.END, ip_addr)
+
+        #Server Port
+        self.port_sv_entry.delete(1.0,tk.END)
+        self.port_sv_entry.insert(tk.END, sv_port)
+
+        #frame Status
+        self.stt_entry.delete(1.0,tk.END)
+        self.stt_entry.insert(tk.END, stt)
+
+        #Client Port
+        self.port_cl_entry.delete(1.0,tk.END)
+        self.port_cl_entry.insert(tk.END, cl_port)
+
+        #frame Log
+        self.log_entry.insert(tk.END, log +'\n')
+
+'''def UI_server(ip_addr, sv_port, cl_port, stt, log):        
     root = Tk()
     root.geometry("500x300+450+200")
     app = Inerface(root)
+    app.initUI(ip_addr, sv_port, cl_port, stt, log)
     root.mainloop()  
 
 #UI_server(ip_addr, sv_port, cl_port, stt, log)
 UI_server("localhost", "81", "89898", "Not conneted", "blah blah")
+UI_server("localhost", "81", "89898", "Not conneted", "blah blah")'''
+     
+root = Tk()
+root.geometry("500x300+450+200")
+app = Inerface(root)
+for i in range (1,100):
+    app.updateUI("localhost", "81", "89898", "Not conneted", "blah blah")
+root.mainloop()  
